2ec61ddc1983838e2b277db1c9320fc5ecd8f9d5
official-techsupport <official_techsupport@protonmail.com>
2022-12-09 08:29:18 +0200

    days 8 and 9

--- advent_of_code2022.py
+++ advent_of_code2022.py
@@ -200,8 +200,6 @@ $ ls
             elif consume('cd'):
                 if s == '/':
                     current = tree
-                elif s == '..':
-                    current = current['..']
                 else:
                     current = current[s]
                     assert not isinstance(current, int)
@@ -235,6 +233,119 @@ $ ls
     to_delete = 30000000 - unused
     # print(to_delete)
     return min(size for size in dirs if size >= to_delete)
+
+
+def problem8(data, second):
+    _data = split_data('''30373
+25512
+65332
+33549
+35390''')
+    h = len(data)
+    w = len(data[0])
+    assert all(len(s) == w for s in data)
+
+    vis = set()
+
+    def dix(pos):
+        return data[pos[0]][pos[1]]
+
+    def go(pos, dir, cnt):
+        tallest = None
+        for _ in range(cnt):
+            h = dix(pos)
+            if not tallest or tallest < h:
+                vis.add(pos)
+                tallest = h
+            pos = addv2(pos, dir)
+
+    def go2(pos, dir):
+        myh = dix(pos)
+        for i in range(1000):
+            pos = addv2(pos, dir)
+            if not (0 <= pos[0] < h) or not (0 <= pos[1] < w):
+                return i
+            if myh <= dix(pos):
+                return i + 1
+        assert False
+
+    for i in range(h):
+        go((i, 0), (0, 1), w)
+        go((i, w - 1), (0, -1), w)
+
+    for i in range(w):
+        go((0, i), (1, 0), h)
+        go((h - 1, i), (-1, 0), h)
+
+    if not second:
+        return len(vis)
+
+    scores = []
+    for i in range(1, h - 1):
+        for j in range(1, w - 1):
+            pos = (i, j)
+            scores.append(
+                go2(pos, (0, 1)) *
+                go2(pos, (0, -1)) *
+                go2(pos, (1, 0)) *
+                go2(pos, (-1, 0)))
+
+    return max(scores)
+
+
+def problem9(data, second):
+    _data = split_data('''R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2''')
+    if second:
+        _data = split_data('''R 4
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20''')
+    snek = [0j] * (10 if second else 2)
+    visited = set([0j])
+
+    def moveh(c, h):
+        return h + {'U': 1j, 'R': 1, 'D': -1j, 'L': -1}[c]
+
+    def _movet(h, t):
+        diff = h - t
+        if abs(diff.real) > 1 and abs(diff.imag) > 1:
+            return (t.real + diff.real / 2) + 1j * (t.imag + diff.imag / 2)
+        elif abs(diff.real) > 1:
+            return (t.real + diff.real / 2) + 1j * h.imag
+        elif abs(diff.imag) > 1:
+            return h.real                   + 1j * (t.imag + diff.imag / 2)
+        return t
+
+    # thx to hbtz
+    def movet(h, t):
+        d = h - t
+        if abs(d.real) > 1 or abs(d.imag) > 1:
+            return t + d.real / max(1, abs(d.real)) + 1j * d.imag / max(1, abs(d.imag))
+        return t
+
+    for s in data:
+        dir, dist = s.split()
+        for _ in range(int(dist)):
+            snek[0] = moveh(dir, snek[0])
+            for i in range(len(snek) - 1):
+                snek[i + 1] = movet(snek[i], snek[i + 1])
+            visited.add(snek[-1])
+            # print(dir, snek)
+
+    return len(visited)
+
 
 
 ##########
