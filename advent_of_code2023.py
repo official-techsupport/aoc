9ea078cb4f8fcbd3676ee00eec029c7974ef8e32
official-techsupport <official_techsupport@protonmail.com>
2023-12-05 06:55:47 +0200

    aoc4

--- advent_of_code2023.py
+++ advent_of_code2023.py
@@ -143,6 +143,40 @@ def problem3(data, second):
     return res
 
 
+def problem4(data, second):
+    _data = split_data('''Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11''')
+    res = []
+    for s in data:
+        _, s = s.split(':')
+        winning, have = s.split('|')
+        def parse(s):
+            return set(map(int, s.split()))
+        winning, have = parse(winning), parse(have)
+        res.append(have & winning)
+    if not second:
+        return sum(0 if not r else 2**(len(r) - 1) for r in res)
+
+    copies = [1] * len(res)
+    for i, r in enumerate(res):
+        for x in range(i + 1, i + len(r) + 1):
+            copies[x] += copies[i]
+    return sum(copies)
+
+
+
+
+
+
+
+    return None
+
+
+
 #########
 
 def problem(data, second):
